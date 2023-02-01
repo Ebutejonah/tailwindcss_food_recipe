7d1577bfcf7944aaa0b31b923b76d014c4f948a7
giftedmatrix <ebutejonah386@gmail.com>
2023-02-01 11:58:35 +0100

    sophisticated darkmode

--- js/main.js
+++ js/main.js
@@ -1,20 +1,50 @@
-const burg = document.getElementById("hamburger")
-const mob_menu= document.getElementById("menu")
+const hamburger = document.getElementById("hamburger")
+const mobile_menu = document.getElementById("menu")
 const darkmode = document.getElementById("toggle")
-const page=document.querySelector("html")
 
-const toggledarkmode= function(){
-    darkmode.checked ? page.classList.add("dark") : page.classList.remove("dark");
+//const page=document.querySelector("html")
+//const toggledarkmode= function(){
+   // darkmode.checked ? page.classList.add("dark") : page.classList.remove("dark");
+//}
+//toggledarkmode();
+//darkmode.addEventListener("click",toggledarkmode);
+const userTheme = localStorage.getItem('theme')
+const systemTheme = window.matchMedia('prefered-color-theme: dark').matches
+
+
+const checkTheme = ()=>{
+    if(userTheme === 'dark' ||(!userTheme && systemTheme)){
+        document.documentElement.classList.add('dark')
+        return
+    }
 }
-toggledarkmode();
-darkmode.addEventListener("click",toggledarkmode);
 
-burg.addEventListener('click', ()=>{
-    if (mob_menu.classList.contains('hidden')){
-        mob_menu.classList.remove('hidden')
+const themeSwitch = ()=>{
+    if(document.documentElement.classList.contains('dark')){
+        document.documentElement.classList.remove('dark')
+        localStorage.setItem('theme', 'Light')
+        return
+    }
+    document.documentElement.classList.add('dark')
+    localStorage.setItem('theme', 'dark')
+}
+
+darkmode.addEventListener('click', ()=>{
+    themeSwitch()
+})
+
+checkTheme()
+
+
+
+
+
+hamburger.addEventListener('click', ()=>{
+    if (mobile_menu.classList.contains('hidden')){
+        mobile_menu.classList.remove('hidden')
     }
     else{
-        mob_menu.classList.add('hidden')
+        mobile_menu.classList.add('hidden')
     }
 })
 
